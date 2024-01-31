4f9a88d90907928486805a6d29874565ca64ac12
Otarion <c.garsault@laposte.net>
2024-01-31 15:02:13 +0100

    Atelier 3 : commentaires (correction)

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // On récupère une instance de la classe App
 $app = MVC\App::getInstance();
+
 // Servira à ajouter mes services au conteneur
 $app->boot();
 
@@ -21,19 +22,3 @@ dd(
     $app->make('request'),                                                   // Affiche une instance du service 'request' depuis le conteneur de dépendances (peut être une gestion de la requête HTTP)
     $routes = require_once __DIR__ .'/../public/routes.php'  // Affiche et récupère la configuration des routes depuis un fichier externe
 );
-
-/*
-  Commentaires explicatifs :
-  
-  1. Ce script PHP sert de point d'entrée principal (front controller) où toutes les requêtes de l'application passent.
-
-  2. L'autoloader de Composer est inclus pour charger automatiquement les classes nécessaires.
-
-  3. Une instance de la classe App est obtenue à l'aide de la méthode statique 'getInstance'. Il semble que la classe App soit un singleton, limitant la création d'une seule instance.
-
-  4. La méthode 'boot' de l'instance de la classe App est appelée. Cela peut impliquer l'initialisation de certains services ou configurations nécessaires à l'application.
-
-  5. Deux services 'foo' et 'bar' sont ajoutés au conteneur de dépendances à l'aide de la méthode 'singleton'. Ces services sont des instances anonymes de classes.
-
-  6. La fonction 'dd' est utilisée pour afficher de manière structurée et arrêter l'exécution du script. Elle affiche l'instance de la classe App, une instance du service 'bar', une instance du service 'request', et récupère la configuration des routes depuis un fichier externe.
-*/
--- public/routes.php
+++ public/routes.php
@@ -11,16 +11,3 @@ return [
     // Définition d'une route pour l'URI '/hello/{name}'
     Route::get('/hello/{name}', [App\Controllers\HelloController::class, 'hello']),
 ];
-
-/*
-  Commentaires explicatifs :
-  
-  1. Ce fichier sert à configurer les routes de l'application web.
-  
-  2. La première route est définie pour l'URI '/', associée à la méthode HTTP GET.
-     Elle pointe vers la méthode 'index' du contrôleur 'HelloController' du namespace 'App\Controllers'.
-
-  3. La deuxième route est définie pour l'URI '/hello/{name}', associée à la méthode HTTP GET.
-     La partie dynamique '{name}' de l'URI permet de capturer un paramètre dans l'URL.
-     Cette route pointe vers la méthode 'hello' du contrôleur 'HelloController' du namespace 'App\Controllers'.
-*/
